8815e8cb8fec6dd3b8141d5055430e573d872045
tainanlehao <124031994+tainanlehao@users.noreply.github.com>
2023-10-19 10:25:11 +0800

    Update basic.vue

--- docs/examples/button/basic.vue
+++ docs/examples/button/basic.vue
@@ -29,7 +29,7 @@
   <el-row>
     <el-button :icon="Search" circle />
     <el-button type="primary" :icon="Edit" circle />
-    <el-button type="success" :icon="Check" circle />
+    <el-button type="success"  circle />
     <el-button type="info" :icon="Message" circle />
     <el-button type="warning" :icon="Star" circle />
     <el-button type="danger" :icon="Delete" circle />
